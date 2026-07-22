a7c9d385b1e3962dd3e42166a0bd2b3db1ba415d
dtbuild <dtbuild@rigil>
2026-07-22 13:41:06 +0000

    9357823b84064d8c7144c51fcce9080bec43ef0c Release 2.0.0
    Sync to source repo @9357823b84064d8c7144c51fcce9080bec43ef0c

--- js/columnControl.bulma.min.mjs
+++ js/columnControl.bulma.min.mjs
@@ -1,4 +1,4 @@
-/*! ColumnControl Bulma styling 2.0.0-beta.1 for DataTables
+/*! ColumnControl Bulma styling 2.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-bm";import ColumnControl from"datatables.net-columncontrol";DataTable.ColumnControl.content.dropdown.classes.container=["dtcc-dropdown","dropdown","dropdown-menu"],DataTable.ColumnControl.content.dropdown.classes.liner=["dtcc-dropdown-liner","dropdown-content"];export default DataTable;
